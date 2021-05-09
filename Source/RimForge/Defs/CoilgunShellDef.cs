22d6cd0c2544c6cb7d37b651f8a4e09b3f5bdbfe
James Billy <james@epicguru.co.uk>
2021-05-09 20:31:38 +0200

    Added HE shell.

--- Source/RimForge/Defs/CoilgunShellDef.cs
+++ Source/RimForge/Defs/CoilgunShellDef.cs
@@ -7,5 +7,11 @@ namespace RimForge
         public float baseDamage = 800;
         public int maxPen = -1;
         public float penDamageMultiplier = 0.95f;
+        public bool pawnsCountAsPen = false;
+
+        public float explosionRadius = 5f;
+        public DamageDef explosionDamageType;
+        public int? explosionDamage;
+        public float? explosionArmorPen;
     }
 }
